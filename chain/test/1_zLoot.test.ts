e0a9bdedfc52e86313b37d0a79bf6ab9170d06e2
Zsofie <zsofia@linumlabs.com>
2021-09-10 11:50:46 +0200

    beforeEach for tests started

--- chain/test/1_zLoot.test.ts
+++ chain/test/1_zLoot.test.ts
@@ -7,12 +7,34 @@ import { BigNumber, Contract, Signer } from "ethers";
 describe("Deployment", () => {
 
 
+    let ZLootContract: Contract;
+    let ZLootInstance: Contract;
+    let ZGldContract: Contract;
+    let ZGldInstance: Contract;
+    let owner: Signer;
+    let alice: Signer;
+    let bob: Signer;
     
+    let ownerAddress: string;
+    let aliceAddress: string;
+    let bobAddress: string;
+
     beforeEach(async () => {
+        [owner, alice, bob, ] = await ethers.getSigners();
+        ownerAddress = await owner.getAddress();
+        aliceAddress = await alice.getAddress();
+        bobAddress = await bob.getAddress();
+        ZLootContract = await ethers.getContractFactory("ZLoot");
+        ZLootInstance = await ZLootContract.connect(owner).deploy();
+        ZGldContract = await ethers.getContractFactory("ZGold");
+        ZGldInstance = await ZLootContract.connect(owner).deploy()
+
+       
+    })
 
     })
 
-    describe("Constructor", () => {
+    describe("Token Basics", () => {
 
         it('sets the name correctly', async () => {
            
@@ -21,10 +43,53 @@ describe("Deployment", () => {
             
         });
 
-        it('sets decimals correctly', async () => {
+        it('', async () => {
             
         })
-        it('sets total supply correctly', async () => {
+        it('', async () => {
+        
+        });
+
+    })
+
+    describe("Randomness", () => {
+
+        it('gives random companion after every minting', async () => {
+           
+        });
+        it('gives random special power after every minting', async () => {
+            
+        });
+
+        it('gives random weapon after every minting', async () => {
+            
+        })
+        it('gives random hand armor after every minting', async () => {
+        
+        });
+
+        it('gives random ring after every minting', async () => {
+        
+        });
+
+    })
+    describe("claiming ZGLD", () => {
+
+        it('', async () => {
+           
+        });
+        it('', async () => {
+            
+        });
+
+        it('', async () => {
+            
+        })
+        it('', async () => {
+        
+        });
+
+        it('', async () => {
         
         });
 
